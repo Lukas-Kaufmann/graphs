4032dc0397dd9d5ad980c4dc40c1d7d8244d3531
Lukas Kaufmann <lukas.kaufm@outlook.com>
2023-04-26 10:06:57 +0200

    read from file

--- src/main/java/at/fhv/lka2/Main.java
+++ src/main/java/at/fhv/lka2/Main.java
@@ -1,28 +1,68 @@
 package at.fhv.lka2;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        AdjacencylistGraph<String, String, String> graph = new AdjacencylistGraph<>();
-        //MatrixGraph<String, String, String> graph = new MatrixGraph<>(2);
 
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
-        graph.addVertex("D");
+    public static AdjacencylistGraph<String, String, Integer> loadGraph(String filename) throws IOException {
+        AdjacencylistGraph<String, String, Integer> graph = new AdjacencylistGraph();
+        Path filePath = Path.of("sample-graph-input.json");
 
-        graph.addEdge("A", "B", new HashMap<>());
-        graph.addDirectedEdge("B", "C", new HashMap<>());
+        String content = Files.readString(filePath);
+        JSONObject obj = new JSONObject(content);
 
-        graph.addDirectedEdge("C", "A", new HashMap<>());
+        JSONArray vertexes = obj.getJSONArray("vertexes");
 
-        graph.removeEdge("A", "C");
+        for (Iterator<Object> it = vertexes.iterator(); it.hasNext(); ) {
+            Object v = it.next();
+            graph.addVertex((String) v);
+        }
 
-        graph.removeVertex("D");
+        JSONArray edges = obj.getJSONArray("edges");
+        for (int i = 0; i < edges.length(); i+=1) {
+            JSONObject edge = edges.getJSONObject(i);
+            Map<String, Object> attributesObj = edge.getJSONObject("attributes").toMap();
+            Map<String, Integer> attributes = new HashMap<>();
+            for (Map.Entry<String, Object> entry : attributesObj.entrySet()) {
+                attributes.put(entry.getKey(), (Integer) entry.getValue());
+            }
+            graph.addDirectedEdge(edge.getString("from"), edge.getString("to"), attributes);
+        }
 
-        System.out.println(graph);
+        return graph;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Graph graph = loadGraph("sample-graph1-input.json");
+        System.out.println(graph.toString());
+
+        AdjacencylistGraph<String, String, String> graph1 = new AdjacencylistGraph<>();
+        //MatrixGraph<String, String, String> graph1 = new MatrixGraph<>(2);
+
+        graph1.addVertex("A");
+        graph1.addVertex("B");
+        graph1.addVertex("C");
+        graph1.addVertex("D");
+
+        graph1.addEdge("A", "B", new HashMap<>());
+        graph1.addDirectedEdge("B", "C", new HashMap<>());
+
+        graph1.addDirectedEdge("C", "A", new HashMap<>());
+
+        graph1.removeEdge("A", "C");
+
+        graph1.removeVertex("D");
+
+        //System.out.println(graph1);
 
     }
 }
